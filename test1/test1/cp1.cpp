b01a0d39f84a8dc8b79202371521fd6adc8309cb
PZLiu <786122008@qq.com>
2018-05-16 23:14:51 +0800

    inital commit

---
+++ test1/test1/cp1.cpp
@@ -0,0 +1,151 @@
+//
+//  cp1.cpp
+//  test1
+//
+//  Created by 刘鹏志 on 2018/4/22.
+//  Copyright © 2018年 刘鹏志. All rights reserved.
+//
+
+/* 1、打印出所有选手的名字与参赛号，并以参赛号的升序排列
+   2、打印每一轮比赛后，小组比赛成绩和小组晋级名单
+   3、打印决赛前三名，选手名称、成绩
+ */
+/*
+  产生选手 姓名、得分、选手编号；
+  第一轮 选手抽签 选手比赛 查看比赛结果
+  第二轮 选手抽签 选手比赛 查看比赛结果
+  第三轮 选手抽签 选手比赛 查看比赛结果
+*/
+ 
+#include <iostream>
+#include "string"
+#include "algorithm"
+#include "functional"
+#include "numeric"
+#include "iterator"
+#include "vector"
+#include "map"
+#include "deque"
+using namespace std;
+
+class Speaker {
+public:
+    string m_name;
+    int    m_score[3];
+};
+
+//产生选手
+int GenSpeaker (map<int, Speaker> &mapSpeaker, vector<int> &v) {
+    
+    string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    random_shuffle(str.begin(), str.end());
+    
+    for (int i=0; i<24; i++) {
+        Speaker temp;
+        temp.m_name = "选手:";
+        temp.m_name += str[i];
+        mapSpeaker.insert(pair<int, Speaker> (100+i, temp));
+    }
+    for (int i=0; i<24; i++) {
+        v.push_back(100+i);
+    }
+    return 0;
+}
+
+
+
+//选手抽签
+int speech_contest_draw (vector<int> &v) {
+    
+    random_shuffle(v.begin(), v.end());
+    return 0;
+}
+
+
+
+//选手比赛
+int speech_contest(int index, vector<int> &v1, map<int, Speaker> &mapSpeaker, vector<int> &v2) {
+    
+    
+    multimap<int, int, greater<int>> multmapGroup;  //小组成绩
+    int tmpcount = 0;
+    
+    for (vector<int>::iterator it=v1.begin(); it!=v1.end() ; it++) {
+        //打分
+        tmpcount ++;
+        deque<int> dscore;
+        for (int j=0; j<10; j++) {       //十个评委打分
+            int score = 50 + rand()%50;  //rand()产生0~49的随机数
+            dscore.push_back(score);
+        }
+        sort(dscore.begin(), dscore.end());
+        dscore.pop_back();
+        dscore.pop_front(); //去掉最低分 和 最高分
+        
+        //求平均分
+        int scoresum = accumulate(dscore.begin(), dscore.end(), 0);
+        int scoreavg = scoresum / dscore.size();
+        mapSpeaker[*it].m_score[index] = scoreavg; //选手得分
+        multmapGroup.insert(pair<int, int>(scoreavg, *it));
+    }
+    
+    // 处理分组
+    if (tmpcount%6 == 0) {
+        cout << "小组的成绩" << endl;
+        for (multimap<int, int, greater<int>>::iterator mit=multmapGroup.begin(); mit != multmapGroup.end(); mit++) {
+            //编号 姓名 得分
+            cout << mit->second << "\t" << mapSpeaker[mit->second].m_name << "\t" << mit->first << endl;
+        }
+        
+        //前三名晋级
+        while (multmapGroup.size() > 3) {
+            multimap<int, int, greater<int>>::iterator it1 = multmapGroup.begin();
+            v2.push_back(it1->second); //把前三名 放到v2晋级名单中
+            multmapGroup.erase(it1);
+        }
+        multmapGroup.clear(); //清空本小组 比赛成绩
+    }
+    return 0;
+}
+
+
+
+//查看比赛结果
+int speech_contest_print(int index, vector<int> &v, map<int, Speaker> &mapSpeaker) {
+    
+    printf("第%d轮，晋级名单\n", index+1);
+    
+    for (vector<int>::iterator it1=v.begin(); it1!=v.end(); it1++) {
+        cout << "参赛编号：" << *it1 << "\t" << mapSpeaker[*it1].m_name << "\t" << mapSpeaker[*it1].m_score[index] << endl;
+    }
+    return 0;
+}
+
+int main () {
+    //容器的设计
+    map<int, Speaker> mapSpeaker;  //参加比赛的选手
+    vector<int>       v1;         //第一轮 演讲比赛 名单
+    vector<int>       v2;         //第二轮 演讲比赛 名单
+    vector<int>       v3;         //第三轮 演讲比赛 名单
+    vector<int>       v4;         //最后三名 演讲比赛 名单
+    
+    //产生选手 得到第一轮选手的比赛名单
+    GenSpeaker (mapSpeaker, v1);
+    
+    //第一轮 选手抽签 选手比赛 查看比赛结果
+    speech_contest_draw(v1);
+    speech_contest(0, v1, mapSpeaker, v2);
+    speech_contest_print(0, v2, mapSpeaker);
+    
+    //第二轮 选手抽签 选手比赛 查看比赛结果
+    speech_contest_draw(v2);
+    speech_contest(1, v2, mapSpeaker, v3);
+    speech_contest_print(1, v3, mapSpeaker);
+    
+    //第三轮 选手抽签 选手比赛 查看比赛结果
+    speech_contest_draw(v3);
+    speech_contest(2, v3, mapSpeaker, v4);
+    speech_contest_print(2, v4, mapSpeaker);
+}
+
+
---
+++ test1/test1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test1
+//
+//  Created by 刘鹏志 on 2018/3/22.
+//  Copyright © 2018年 刘鹏志. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
